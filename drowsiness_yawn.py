35d077d907e8d181f4fe43e11f1bd7981dc85890
Harsanjam <56749102+Harsanjam@users.noreply.github.com>
2023-12-17 01:05:49 -0500

    Update drowsiness_yawn.py

--- drowsiness_yawn.py
+++ drowsiness_yawn.py
@@ -1,5 +1,3 @@
-#python drowniness_yawn.py --webcam webcam_index
-
 from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
@@ -12,158 +10,158 @@ import dlib
 import cv2
 import os
 
-def alarm(msg):
-    global alarm_status
-    global alarm_status2
-    global saying
+# Function to sound alarm or speak a message
+def sound_alarm(message):
+    global alarm_active
+    global alarm_active2
+    global speaking
 
-    while alarm_status:
-        print('call')
-        s = 'espeak "'+msg+'"'
-        os.system(s)
+ # If alarm is active, continuously sound the alarm
+    while alarm_active:
+        print('Calling...')
+        speech_command = 'espeak "' + message + '"'
+        os.system(speech_command)
 
-    if alarm_status2:
-        print('call')
-        saying = True
-        s = 'espeak "' + msg + '"'
-        os.system(s)
-        saying = False
+  # If the second alarm is active and not currently speaking, deliver the message
+    if alarm_active2:
+        print('Calling...')
+        speaking = True
+        speech_command = 'espeak "' + message + '"'
+        os.system(speech_command)
+        speaking = False
 
-def eye_aspect_ratio(eye):
-    A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
+def calculate_eye_aspect_ratio(eye_landmarks):
+    A = dist.euclidean(eye_landmarks[1], eye_landmarks[5])
+    B = dist.euclidean(eye_landmarks[2], eye_landmarks[4])
+    C = dist.euclidean(eye_landmarks[0], eye_landmarks[3])
 
-    C = dist.euclidean(eye[0], eye[3])
+    eye_ratio = (A + B) / (2.0 * C)
+    return eye_ratio
 
-    ear = (A + B) / (2.0 * C)
+def calculate_final_eye_aspect_ratio(facial_shape):
+    left_eye_start, left_eye_end = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    right_eye_start, right_eye_end = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-    return ear
+    left_eye_landmarks = facial_shape[left_eye_start:left_eye_end]
+    right_eye_landmarks = facial_shape[right_eye_start:right_eye_end]
 
-def final_ear(shape):
-    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    left_eye_ratio = calculate_eye_aspect_ratio(left_eye_landmarks)
+    right_eye_ratio = calculate_eye_aspect_ratio(right_eye_landmarks)
 
-    leftEye = shape[lStart:lEnd]
-    rightEye = shape[rStart:rEnd]
+    eye_aspect_ratio = (left_eye_ratio + right_eye_ratio) / 2.0
+    return (eye_aspect_ratio, left_eye_landmarks, right_eye_landmarks)
 
-    leftEAR = eye_aspect_ratio(leftEye)
-    rightEAR = eye_aspect_ratio(rightEye)
+def calculate_lip_distance(facial_shape):
+    top_lip = facial_shape[50:53]
+    top_lip = np.concatenate((top_lip, facial_shape[61:64]))
 
-    ear = (leftEAR + rightEAR) / 2.0
-    return (ear, leftEye, rightEye)
-
-def lip_distance(shape):
-    top_lip = shape[50:53]
-    top_lip = np.concatenate((top_lip, shape[61:64]))
-
-    low_lip = shape[56:59]
-    low_lip = np.concatenate((low_lip, shape[65:68]))
+    bottom_lip = facial_shape[56:59]
+    bottom_lip = np.concatenate((bottom_lip, facial_shape[65:68]))
 
     top_mean = np.mean(top_lip, axis=0)
-    low_mean = np.mean(low_lip, axis=0)
+    bottom_mean = np.mean(bottom_lip, axis=0)
 
-    distance = abs(top_mean[1] - low_mean[1])
+    distance = abs(top_mean[1] - bottom_mean[1])
     return distance
 
-
+# Command-line arguments setup
 ap = argparse.ArgumentParser()
 ap.add_argument("-w", "--webcam", type=int, default=0,
                 help="index of webcam on system")
 args = vars(ap.parse_args())
 
-
-
-EYE_AR_THRESH = 0.3
+# Constants for eye and yawn detection thresholds
+EYE_AR_THRESHOLD = 0.3
 EYE_AR_CONSEC_FRAMES = 30
-YAWN_THRESH = 17
-alarm_status = True
-alarm_status2 = True
-saying = True
-COUNTER = 0
+YAWN_THRESHOLD = 17
+alarm_active = True
+alarm_active2 = True
+speaking = True
+FRAME_COUNTER = 0
 
+# Loading the face detector and facial landmark predictor
 print("-> Loading the predictor and detector...")
-#detector = dlib.get_frontal_face_detector()
-detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")    #Faster but less accurate
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+shape_predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-
+# Starting video stream
 print("-> Starting Video Stream")
-vs = VideoStream(src=args["webcam"]).start()
-#vs= VideoStream(usePiCamera=True).start()       //For Raspberry Pi
+video_stream = VideoStream(src=args["webcam"]).start()
 time.sleep(1.0)
 
 while True:
-
-    frame = vs.read()
+    frame = video_stream.read()
     frame = imutils.resize(frame, width=450)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #rects = detector(gray, 0)
-    rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+# Detect faces in the grayscale frame
+    detected_faces = face_detector.detectMultiScale(gray, scaleFactor=1.1, 
 		minNeighbors=5, minSize=(30, 30),
 		flags=cv2.CASCADE_SCALE_IMAGE)
 
-    #for rect in rects:
-    for (x, y, w, h) in rects:
-        rect = dlib.rectangle(int(x), int(y), int(x + w),int(y + h))
-        
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+    for (x, y, w, h) in detected_faces:
+        face_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+        facial_shape = shape_predictor(gray, face_rect)
+        facial_shape = face_utils.shape_to_np(facial_shape)
 
-        eye = final_ear(shape)
-        ear = eye[0]
-        leftEye = eye [1]
-        rightEye = eye[2]
+ # Calculate eye aspect ratio and lip distance
+        eye = calculate_final_eye_aspect_ratio(facial_shape)
+        eye_aspect_ratio = eye[0]
+        left_eye = eye[1]
+        right_eye = eye[2]
 
-        distance = lip_distance(shape)
+        lip_distance_value = calculate_lip_distance(facial_shape)
 
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+# Visualize eye and lip contours
+        left_eye_hull = cv2.convexHull(left_eye)
+        right_eye_hull = cv2.convexHull(right_eye)
+        cv2.drawContours(frame, [left_eye_hull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [right_eye_hull], -1, (0, 255, 0), 1)
 
-        lip = shape[48:60]
-        cv2.drawContours(frame, [lip], -1, (0, 255, 0), 1)
+        lip_contour = facial_shape[48:60]
+        cv2.drawContours(frame, [lip_contour], -1, (0, 255, 0), 1)
 
-        if ear < EYE_AR_THRESH:
-            COUNTER += 1
+  # Check for drowsiness and yawn alerts based on thresholds
+        if eye_aspect_ratio < EYE_AR_THRESHOLD:
+            FRAME_COUNTER += 1
 
-            if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                if alarm_status == False:
-                    alarm_status = True
-                    t = Thread(target=alarm, args=('wake up sir',))
-                    t.deamon = True
-                    t.start()
+            if FRAME_COUNTER >= EYE_AR_CONSEC_FRAMES:
+                if not alarm_active:
+                    alarm_active = True
+                    alarm_thread = Thread(target=sound_alarm, args=('Wake up, please!',))
+                    alarm_thread.daemon = True
+                    alarm_thread.start()
 
                 cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
         else:
-            COUNTER = 0
-            alarm_status = False
+            FRAME_COUNTER = 0
+            alarm_active = False
 
-        if (distance > YAWN_THRESH):
+        if lip_distance_value > YAWN_THRESHOLD:
                 cv2.putText(frame, "Yawn Alert", (10, 30),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                if alarm_status2 == False and saying == False:
-                    alarm_status2 = True
-                    t = Thread(target=alarm, args=('take some fresh air sir',))
-                    t.deamon = True
-                    t.start()
+                if not alarm_active2 and not speaking:
+                    alarm_active2 = True
+                    alarm_thread = Thread(target=sound_alarm, args=('Take some fresh air, please!',))
+                    alarm_thread.daemon = True
+                    alarm_thread.start()
         else:
-            alarm_status2 = False
+            alarm_active2 = False
 
-        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+# Display eye aspect ratio and lip distance values on the frame
+        cv2.putText(frame, "EAR: {:.2f}".format(eye_aspect_ratio), (300, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "YAWN: {:.2f}".format(distance), (300, 60),
+        cv2.putText(frame, "YAWN: {:.2f}".format(lip_distance_value), (300, 60),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-
+    # Display the processed frame
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
+# Exit loop when 'q' key is pressed
     if key == ord("q"):
         break
 
 cv2.destroyAllWindows()
-vs.stop()
+video_stream.stop()
